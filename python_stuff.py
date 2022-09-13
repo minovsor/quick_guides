f36b21a6e4da84604522359c9955be0c9736c1a2
minovsor <minovsor@users.noreply.github.com>
2022-09-13 12:28:03 -0300

    Update python_stuff.py

--- python_stuff.py
+++ python_stuff.py
@@ -3,3 +3,6 @@
 target_column = codigo
 target_values = [c1,c2]
 df_selected = df.iloc[pd.Index(df[target_column]).get_indexer(target_values)]
+
+# oneline quicksort
+quicks = lambda l: quicks([x for x in l[1:] if x <= l[0]]) + [l[0]] + quicks([x for x in l if x > l[0]]) if l else []
